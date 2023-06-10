f5fbc7c4c268a7163a08ad425c53572c331b3feb
shubhat33 <rashidshubhat41@gmail.com>
2023-06-10 11:51:02 +0530

    got the authentication working

--- app.js
+++ app.js
@@ -9,20 +9,17 @@ app.use(express.static("public"));
 
 mongoose.connect('mongodb+srv://shubhatRashid:Ilovecoding@cluster0.vus6dpg.mongodb.net/todoListDB');
 var isLoggedIn = false
-var userName = ''
-const itemSchema = mongoose.Schema({
-  name:String
-})
-const Item = mongoose.model("Item",itemSchema)
-
+var userMail = ''
 const listSchema = mongoose.Schema({
-  name : String,
-  items : []
-})
+    listName : String,
+    items : Array
+  })
+
 const userSchema = mongoose.Schema({
   name : String,
   email : String,
-  Password : String
+  Password : String,
+  lists : [listSchema]
 })
 const List = mongoose.model("List",listSchema)
 const User = mongoose.model("User",userSchema)
@@ -32,27 +29,24 @@ const defaultArray = [{name:"Hi,Write and Press +"},{name:"To delete check and p
 app.get("/",function(req,res){
 
   if (isLoggedIn){
-    const customListName = _.upperCase("Today")
 
-    List.findOne({name:customListName}).then(result => {
-
-      if (result === null){
-          const list = new List({
-          name:customListName,
-          items:defaultArray
-         })
-         list.save()
-         res.redirect("/"+customListName)
-      }else{
-        List.find({}).then(Result =>{
-          res.render("lists.ejs",{titleItem:customListName,listTypes:Result,nextItem:result.items})
-        })
-         
-      }
-   })
+    User.findOne({email:userMail}).then(
+      result => {
+        User.find({}).then(Result =>{
+                  res.render("lists.ejs",{titleItem:"Today",listTypes:result.lists,nextItem:result.lists})
+                })
+      })
   }else{
     res.render("SignUp.ejs")
   }
+})
+
+app.post("/",function(req,res){
+  User.updateOne({email:userMail,"lists.listName" :req.body.list}, {
+    '$push': {
+       "lists.$.items": {name: req.body.say}
+    }
+  }).then(res.redirect("/"+req.body.list))
 })
 
 app.get('/signup',(req,res) =>{
@@ -63,7 +57,10 @@ app.post('/signUp',(req,res) =>{
   const user = new User({
     name : req.body.userName,
     email:req.body.email,
-    Password : req.body.Password
+    Password : req.body.Password,
+    lists :[{
+      listName:"Today",
+      items:defaultArray}]
   })
   user.save()
   res.render("login.ejs")
@@ -73,6 +70,7 @@ app.get('/login',(req,res) =>{
   res.render("login.ejs")
 })
 
+
 app.post("/login",(req,res)=>{
   User.findOne({email:req.body.email,Password:req.body.password}).then(
     result => {
@@ -80,86 +78,53 @@ app.post("/login",(req,res)=>{
         res.render("login.ejs")
       }else{
         isLoggedIn = true
+        userMail = req.body.email
         res.redirect("/")
       }
     }
   )
 })
 
+app.post("/delete",(req,res)=>{
+  User.findOneAndUpdate({email:userMail,"lists.listName" :req.body.ListName}, {
+    '$pull': {
+       "lists.$.items": {name: req.body.checkbox}
+    }
+  }).then(res.redirect("/"))
+})
 
+app.post("/newList", (req,res) =>{
+  User.updateOne({email:userMail}, {
+    '$push': {
+       lists: {
+        listName:req.body.newList,
+        items:defaultArray}
+    }
+  }).then(res.redirect("/"))
+})
 
 app.get("/:name",function(req,res){
-  // console.log(req.params.name)
-    const customListName = _.upperCase(req.params.name)
-
-    List.findOne({name:customListName}).then(result => {
-
-      if (result === null){
-          const list = new List({
-          name:customListName,
-          items:defaultArray
-         })
-         list.save()
-         res.redirect("/"+customListName)
-      }else{
-        List.find({}).then(Result =>{
-          res.render("lists.ejs",{titleItem:customListName,listTypes:Result,nextItem:result.items})
-       })
-      }
-   })
-
-})
-
-app.get("/about",function(req,res){
-    res.render("about.ejs")
-})
-
-app.post("/delete",function(req,res){
-  var id = req.body.checkbox
-  var ListName = req.body.ListName
-  
-  List.findOneAndUpdate({name:ListName},{$pull:{items:{name:id}}}).then(err=>{
-    console.log(err)
-    res.redirect("/"+ListName)
-  })
-  
-
-})
-
-app.post("/",function(req,res){
-    var newItem = req.body.insertMany
-    var newList = req.body.list
-    newItem = new Item ({
-      name : req.body.say
+  if (isLoggedIn){
+  User.findOne({email:userMail}).then(
+    result => {
+      User.find({}).then(Result =>{
+                res.render("lists.ejs",{titleItem:req.params.name,listTypes:result.lists,nextItem:result.lists})
+              })
     })
-
-    
-    List.findOne({name:newList}).then(result => {
-      result.items.push(newItem)
-      result.save()
-      res.redirect("/"+ newList)
-    })
+  }else{
+    res.render()
+  }
 })
 
-app.post("/deleteList",function(req,res){
-  var ListName = req.body.checkbox
-
-  List.deleteOne({name:ListName}).then(err=>{
-    console.log(err)
-    res.redirect("/")
-  })
-  
-
+app.post("/deleteList",(req,res)=>{
+  User.findOneAndUpdate({email:userMail,"lists.listName" :req.body.ListName}, {
+    '$pull': {
+       lists: {listName:req.body.checkbox}
+    }
+  }, {new: true}).then(res.redirect("/"))
 })
 
-app.post("/newList",function(req,res){
-  newListName = req.body.newList
-   res.redirect("/"+newListName)
-  
-})
-
-
-
+/
 
 app.listen(3400,function(){
     console.log("server up and running , listening to port 3400")

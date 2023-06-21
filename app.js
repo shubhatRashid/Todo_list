7a6f667b22ed0dcbfa9ffadbe80256d78d0f262e
shubhat33 <rashidshubhat41@gmail.com>
2023-06-21 21:07:53 +0530

    loginSignUp completed

--- app.js
+++ app.js
@@ -3,13 +3,25 @@ const bodyParser = require("body-parser")
 const app = express()
 const mongoose = require("mongoose")
 const _ = require("lodash")
+const session = require('express-session')
+const passport = require('passport')
+const passportLocalMongoose = require('passport-local-mongoose')
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"));
 
+app.use(session({
+  secret: 'power talks to power',
+  resave: false,
+  saveUninitialized: false
+}))
+
+app.use(passport.initialize());
+app.use(passport.session())
+
 mongoose.connect('mongodb+srv://shubhatRashid:Ilovecoding@cluster0.vus6dpg.mongodb.net/todoListDB');
-var isLoggedIn = false
-var userMail = ''
+
 const listSchema = mongoose.Schema({
     listName : String,
     items : Array
@@ -21,28 +33,35 @@ const userSchema = mongoose.Schema({
   Password : String,
   lists : [listSchema]
 })
+
+userSchema.plugin(passportLocalMongoose);
+
 const List = mongoose.model("List",listSchema)
 const User = mongoose.model("User",userSchema)
+
+passport.use(User.createStrategy())
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 const defaultArray = [{name:"Hi,Write and Press +"},{name:"To delete check and press delete"}]
 
 app.get("/",function(req,res){
 
-  if (isLoggedIn){
+  if (req.isAuthenticated()){
 
-    User.findOne({email:userMail}).then(
+    User.findOne({name:req.user.username}).then(
       result => {
         User.find({}).then(Result =>{
                   res.render("lists.ejs",{titleItem:"Today",listTypes:result.lists,nextItem:result.lists})
                 })
       })
   }else{
-    res.render("SignUp.ejs")
+    res.render("login.ejs")
   }
 })
 
 app.post("/",function(req,res){
-  User.updateOne({email:userMail,"lists.listName" :req.body.list}, {
+  User.updateOne({name:req.user.username,"lists.listName" :req.body.list}, {
     '$push': {
        "lists.$.items": {name: req.body.say}
     }
@@ -54,16 +73,22 @@ app.get('/signup',(req,res) =>{
 })
 
 app.post('/signUp',(req,res) =>{
-  const user = new User({
+  User.register({
+    username:req.body.userName,
     name : req.body.userName,
     email:req.body.email,
-    Password : req.body.Password,
+    password : "",
     lists :[{
       listName:"Today",
       items:defaultArray}]
+  },req.body.Password,function(err,user){
+    if (err){
+      console.log(err)
+      res.redirect("/signup")
+    }else{
+      res.redirect("/")
+    }
   })
-  user.save()
-  res.render("login.ejs")
 })
 
 app.get('/login',(req,res) =>{
@@ -72,21 +97,25 @@ app.get('/login',(req,res) =>{
 
 
 app.post("/login",(req,res)=>{
-  User.findOne({email:req.body.email,Password:req.body.password}).then(
-    result => {
-      if (result === null){
-        res.render("login.ejs")
-      }else{
-        isLoggedIn = true
-        userMail = req.body.email
+
+  const user = new User({
+    username : req.body.username,
+    password: req.body.password
+  }); 
+
+  req.login(user,function(err){
+    if (err) {
+      console.log(err)
+    }else{
+      passport.authenticate("local")(req,res,function(){
         res.redirect("/")
-      }
+      })
     }
-  )
+  })
 })
 
 app.post("/delete",(req,res)=>{
-  User.findOneAndUpdate({email:userMail,"lists.listName" :req.body.ListName}, {
+  User.findOneAndUpdate({name:req.user.username,"lists.listName" :req.body.ListName}, {
     '$pull': {
        "lists.$.items": {name: req.body.checkbox}
     }
@@ -94,7 +123,7 @@ app.post("/delete",(req,res)=>{
 })
 
 app.post("/newList", (req,res) =>{
-  User.updateOne({email:userMail}, {
+  User.updateOne({name:req.user.username}, {
     '$push': {
        lists: {
         listName:req.body.newList,
@@ -104,27 +133,31 @@ app.post("/newList", (req,res) =>{
 })
 
 app.get("/:name",function(req,res){
-  if (isLoggedIn){
-  User.findOne({email:userMail}).then(
+  if (req.isAuthenticated() && req.params.name === "logout"){
+    req.logout(function(err) {
+      if (err) { return next(err); }
+      res.redirect('/');
+    });
+  }
+  else if (req.isAuthenticated()){
+  User.findOne({name:req.user.username}).then(
     result => {
       User.find({}).then(Result =>{
                 res.render("lists.ejs",{titleItem:req.params.name,listTypes:result.lists,nextItem:result.lists})
               })
     })
   }else{
-    res.render()
+    res.redirect("/")
   }
 })
 
 app.post("/deleteList",(req,res)=>{
-  User.findOneAndUpdate({email:userMail,"lists.listName" :req.body.ListName}, {
+  User.findOneAndUpdate({name:req.user.username,"lists.listName" :req.body.ListName}, {
     '$pull': {
        lists: {listName:req.body.checkbox}
     }
   }, {new: true}).then(res.redirect("/"))
 })
-
-/
 
 app.listen(3400,function(){
     console.log("server up and running , listening to port 3400")
